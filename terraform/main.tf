ebb933953fbb1183c184185496bc1488c7a7410f
Leon Funnell <leon_funnell@hotmail.com>
2024-10-03 15:41:38 +0100

    new stuff

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,16 +1,8 @@
 # terraform/main.tf
-resource "random_pet" "bucket" {
-  byte_length = 8
-}
-
-resource "google_storage_bucket" "tf_state" {
-  name     = "tf_state_${random_pet.bucket}"
-  location = var.GCP_REGION
-}
 
 terraform {
   backend "gcs" {
-    bucket  = "tf_state"
+    bucket  = var.TF_STATE_BUCKET
     prefix  = "terraform/state"
   }
 }
