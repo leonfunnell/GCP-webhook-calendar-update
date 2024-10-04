6e52530a3fe035d1f73e2ebacde6aad3c800e931
Leon Funnell <leon_funnell@hotmail.com>
2024-10-04 15:09:55 +0100

    new stuff

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,3 @@
-# terraform/main.tf
-
 provider "google" {
   project     = var.GCP_PROJECT_ID
   region      = var.GCP_REGION
@@ -8,6 +6,39 @@ provider "google" {
 provider "google-beta" {
   project     = var.GCP_PROJECT_ID
   region      = var.GCP_REGION
+}
+
+resource "google_service_account" "my_calendar_sa" {
+  account_id   = var.CALENDAR_SERVICE_ACCOUNT_NAME
+  display_name = "Calendar Service Account"
+}
+
+resource "google_secret_manager_secret" "my_calendar_secret" {
+  secret_id = var.CALENDAR_SECRET_NAME
+  replication {
+    auto {}
+  }
+}
+
+resource "google_secret_manager_secret_version" "my_calendar_secret_version" {
+  secret      = google_secret_manager_secret.my_calendar_secret.id
+  secret_data = google_service_account_key.my_calendar_sa_key.private_key
+}
+
+resource "google_service_account_key" "my_calendar_sa_key" {
+  service_account_id = google_service_account.my_calendar_sa.name
+}
+
+resource "google_project_iam_member" "function_invoker_role" {
+  project = var.GCP_PROJECT_ID
+  role    = "roles/cloudfunctions.invoker"
+  member  = "serviceAccount:${google_service_account.my_calendar_sa.email}"
+}
+
+resource "google_project_iam_member" "calendar_admin_role" {
+  project = var.GCP_PROJECT_ID
+  role    = "roles/calendar.admin"
+  member  = "serviceAccount:${google_service_account.my_calendar_sa.email}"
 }
 
 resource "google_cloudfunctions_function" "webhook_function" {
@@ -20,7 +51,7 @@ resource "google_cloudfunctions_function" "webhook_function" {
   environment_variables = {
     GOOGLE_DEFAULT_CALENDAR_ID = var.GOOGLE_DEFAULT_CALENDAR_ID
     HEADER_SOURCE_TO_PASS = var.HEADER_SOURCE_TO_PASS
-    GCP_SERVICE_ACCOUNT_SECRET = var.GOOGLE_CREDENTIALS
+    GCP_SERVICE_ACCOUNT_SECRET = google_secret_manager_secret_version.my_calendar_secret_version.secret_data
   }
 }
 
@@ -32,7 +63,7 @@ resource "google_storage_bucket" "function_source" {
 resource "google_storage_bucket_object" "function_source" {
   name   = "function-source.zip"
   bucket = google_storage_bucket.function_source.name
-  source = "functions/*"
+  source = "${path.module}/functions/function-source.zip"
 }
 
 resource "google_api_gateway_api" "api_gateway" {
@@ -45,8 +76,8 @@ resource "google_api_gateway_api_config" "api_config" {
   api       = google_api_gateway_api.api_gateway.api_id
   openapi_documents {
     document {
-      path     = "${path.module}/openapi.yaml"
-      contents = filebase64("${path.module}/openapi.yaml")
+      path     = "${path.module}/terraform/openapi.yaml"
+      contents = filebase64("${path.module}/terraform/openapi.yaml")
     }
   }
 }
@@ -69,10 +100,10 @@ variable "GCP_REGION" {
   type        = string
 }
 
-# Variable Declarations
 variable "GOOGLE_CREDENTIALS" {
   description = "The GCP credentials JSON object"
   type        = string
+  sensitive   = true
 }
 
 variable "GOOGLE_DEFAULT_CALENDAR_ID" {
@@ -82,5 +113,15 @@ variable "GOOGLE_DEFAULT_CALENDAR_ID" {
 
 variable "HEADER_SOURCE_TO_PASS" {
   description = "The header source to pass"
+  type        = string
+}
+
+variable "CALENDAR_SERVICE_ACCOUNT_NAME" {
+  description = "The name of the calendar service account"
+  type        = string
+}
+
+variable "CALENDAR_SECRET_NAME" {
+  description = "The name of the calendar secret"
   type        = string
 }
