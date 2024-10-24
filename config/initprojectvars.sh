5cf2fb05f160b5a1a9d4f9ff2e11b1449ead1b2f
Leon Funnell <leon_funnell@hotmail.com>
2024-10-24 11:42:29 +0100

    Delete config/initprojectvars.sh

--- config/initprojectvars.sh
+++
@@ -1,8 +0,0 @@
-export GCP_PROJECT_NAME="My Calendar Project"
-export GOOGLE_APPSHEET_APP_ID="79365135-4f33-4b6f-9c5e-dce60bdb2b0a"
-export GOOGLE_APPSHEET_ACCESS_KEY="V2-ynT6L-xiUtO-QkMOX-p8bk9-Mf4zW-Sg6qS-6OsgI-DqUGB"
-export HEADER_SOURCE_TO_PASS="x-webhook-source:roofbox-webhook-router"
-export GCP_REGION=europe-west2
-export GOOGLE_DEFAULT_CALENDAR_ID="hire@gravesendroofboxhire.com"
-export CALENDAR_SERVICE_ACCOUNT_NAME="calendar_function_sa"
-export CALENDAR_SERVICE_ACCOUNT_DISPLAY_NAME="Calendar Function Service Account"
